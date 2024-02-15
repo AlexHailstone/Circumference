b8ea8170afbb164d8ddf86e270cc786430206435
Solex <alexander.ramsbey@gmail.com>
2024-02-14 20:49:26 -0500

    updated comments and fixed inputs.

--- COS-101_PA4a_AR.py
+++ COS-101_PA4a_AR.py
@@ -7,12 +7,22 @@
 # print(circumference(7))
 ## 43.982297150257104
 
+# Require the math module
+import math
 
+# Set a class
 class CircumferenceCalc:
+    
+    # define my circumference function
     def circumference(radius):
+        
+        # take an overload 'radius' and return the function by multiplying it out
         return 2 * math.pi * radius
 
-    # Sample runs
-    print(circumference(4))  # Expected: 25.132741228718345
-    print(circumference(7))  # Expected: 43.982297150257104
+    # Prompt the user at the console, ensure it's a float and set this as the radius variable
+    radius = float(input("Please enter a radius to calculate the circumference: "))
+
+    # print out the method from your user input as the radius overload
+    print(circumference(radius))
+
         
